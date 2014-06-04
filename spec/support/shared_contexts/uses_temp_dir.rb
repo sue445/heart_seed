9fcf65f0a2470ef2e411c9afcb6807ef9eb32fd8
sue445 <sue445@sue445.net>
2014-06-05 08:26:32 +0900

    Refactor: uses_temp_dir

--- spec/support/shared_contexts/uses_temp_dir.rb
+++ spec/support/shared_contexts/uses_temp_dir.rb
@@ -1,7 +1,10 @@
 shared_context :uses_temp_dir do
-  let!(:temp_dir){ Dir.mktmpdir("rspec-") }
-
-  after do
-    FileUtils.rm_rf(temp_dir)
+  around do |example|
+    Dir.mktmpdir("rspec-") do |dir|
+      @temp_dir = dir
+      example.run
+    end
   end
+
+  attr_reader :temp_dir
 end
