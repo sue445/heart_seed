10acbaaac132abf357cce3b0e3079e2af0debb17
sue445 <sue445@sue445.net>
2014-06-16 08:38:12 +0900

    add to dummy

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+HeartSeed::DbSeed.import_all(tables: ENV["TABLES"])
